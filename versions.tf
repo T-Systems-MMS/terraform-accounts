4d8389dbdf893cb76c3299d0a85f0966ebacab8a
michaelamattes <41573722+michaelamattes@users.noreply.github.com>
2024-08-28 12:35:59 +0200

    pin max version

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
-      version = ">=3.6"
+      version = ">=3.6, <4.0"
     }
     azuread = {
       source  = "registry.terraform.io/hashicorp/azuread"
-      version = ">=2.22"
+      version = ">=2.22, <3.0"
     }
   }
   required_version = ">=1.0"
